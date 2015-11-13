996629b5cd39a35b62427cfc74e12533fd613447
Zhao Yun <zjlyzhy@gmail.com>
2015-11-13 17:59:08 +0800

    add configuration for org-mode such as bindings

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,5 +1,42 @@
+;;; org-mode
 (el-get-bundle org-mode)
 
+(add-to-list 'auto-mode-alist '("\\.org'" . org-mode))
 
+;; suggested bindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; turn on font-lock in org buffer
+(add-hook 'org-mode-hook 'turn-on-font-lock)
+
+;; code blocks
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (python     . t)
+   (sh         . t)
+   (org        . t)
+   (ruby       . t)
+   (latex      . t)
+   (sql        . t)
+   (C          . t)))
+
+(setq org-confirm-babel-evaluate nil)
+
+;; org latex listings
+(require 'ox-latex)
+(add-to-list 'org-latex-packages-alist '("" "ctex"))
+(add-to-list 'org-latex-packages-alist '("" "listings"))
+(add-to-list 'org-latex-packages-alist '("" "color"))
+
+;; code block fontification
+(setq org-src-fontify-natively t
+      org-src-tab-acts-natively t)
+
+;; latex compiler
+(setq org-latex-compiler "xelatex")
 
 (provide 'init-org)
