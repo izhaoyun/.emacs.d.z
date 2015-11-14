20631b980ec24648ef19ba31e6b9958c5b66afe2
Zhao Yun <zjlyzhy@gmail.com>
2015-11-15 02:28:18 +0800

    add cmake support

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -10,4 +10,17 @@
 (define-key c-mode-map   (kbd "M-o")  'fa-show)
 (define-key c++-mode-map (kbd "M-o")  'fa-show)
 
+;; Package: CMake-mode
+(el-get-bundle cmake-mode)
+(require 'cmake-mode)
+(setq auto-mode-alist
+      (append '(("CMakeLists\\.txt\\'" . cmake-mode)
+                ("\\.cmake\\'"         . cmake-mode))
+              auto-mode-alist))
+
+;; Package: CMake-font-lock
+(el-get-bundle cmake-font-lock)
+(autoload 'cmake-font-lock-activate "cmake-font-lock" nil t)
+(add-hook 'cmake-mode-hook 'cmake-font-lock-activate)
+
 (provide 'init-cc)
