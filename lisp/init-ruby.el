ef70748d64f56577eb078d66a39d99c2a8c8255e
Zhao Yun <zjlyzhy@gmail.com>
2015-11-26 10:26:36 +0800

    add support for ruby: init-ruby.el

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -1,1 +1,17 @@
+(require 'init-company)
+(require 'ruby-mode)
+
+;; Package: robe
+(el-get-bundle robe)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+
+;; Package: Enhanced-Ruby-Mode
+(el-get-bundle Enhanced-Ruby-Mode)
+
+;; Package: inf-ruby
+(el-get-bundle inf-ruby)
+(autoload 'inf-ruby-minor-mode "inf-ruby" "Run an inferior Ruby process" t)
+(add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
+
 (provide 'init-ruby)
