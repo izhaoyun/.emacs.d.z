b5531e16d581ca6191acb333ea8f0a70cedf6123
Zhao Yun <zjlyzhy@gmail.com>
2015-11-26 12:07:35 +0800

    add anaconda-mode and company-anaconda

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,7 +1,17 @@
+(require 'init-company)
+
 ;; Package: python-mode
 (el-get-bundle python-mode)
 (require 'python-mode)
 (setq-default py-shell-name "ipython")
 (setq-default py-which-bufname "IPython")
+
+;; Package: anaconda-mode
+(el-get-bundle anaconda-mode)
+(add-hook 'python-mode-hook 'anaconda-mode)
+
+;; Package: company-anaconda
+(el-get-bundle company-anaconda)
+(add-to-list 'company-backends 'company-anaconda)
 
 (provide 'init-python)
