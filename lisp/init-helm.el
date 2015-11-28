d152701086d31d92b8b575d9ce18b80a827caf65
Zhao Yun <zjlyzhy@gmail.com>
2015-11-28 22:57:10 +0800

    modify keybindings for helm

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -16,12 +16,12 @@
 (global-set-key (kbd "M-y")     'helm-show-kill-ring)
 
 ;; fuzzy matching
-(setq helm-recentf-fuzzy-match    t     ; helm-recentf & helm-mini
-      helm-buffers-fuzzy-matching t     ; helm-mini & helm-buffers-list
-      helm-imenu-fuzzy-match      t     ; helm-imenu
-      helm-M-x-fuzzy-match        t     ; helm-M-x
-      helm-apropos-fuzzy-match    t     ; helm-apropos
-      helm-locate-fuzzy-match     t)    ; helm-locate
+(setq helm-recentf-fuzzy-match    t    ; helm-recentf & helm-mini
+      helm-buffers-fuzzy-matching t    ; helm-mini & helm-buffers-list
+      helm-imenu-fuzzy-match      t    ; helm-imenu
+      helm-M-x-fuzzy-match        t    ; helm-M-x
+      helm-apropos-fuzzy-match    t    ; helm-apropos
+      helm-locate-fuzzy-match     t)   ; helm-locate
 
 ;; Package: helm-ag
 (el-get-bundle helm-ag)
@@ -31,16 +31,16 @@
 (el-get-bundle helm-ls-git)
 (require 'helm-ls-git)
 (global-set-key (kbd "C-<f6>") 'helm-ls-git-ls)
-(global-set-key (kbd "C-x C-d") 'helm-browse-project)
+(global-set-key (kbd "C-c C-d") 'helm-browse-project)
 
 ;; Package: helm-swoop
 (el-get-bundle helm-swoop)
 (require 'helm-swoop)
 ;; change the keybinds
-(global-set-key (kbd "M-i") 'helm-swoop)
-(global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+(global-set-key (kbd "C-c M-s") 'helm-swoop)
+(global-set-key (kbd "C-c M-l") 'helm-swoop-back-to-last-point)
 (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
-(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+(global-set-key (kbd "C-c M-I") 'helm-multi-swoop-all)
 
 ;; Package: helm-descbinds
 (el-get-bundle helm-descbinds)
