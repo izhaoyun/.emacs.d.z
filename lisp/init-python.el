ae541bf15770d9455b7e5b2e1fbb7a61fa1d2e9c
Zhao Yun <zjlyzhy@gmail.com>
2015-12-15 00:42:48 +0800

    disable rope temporarily

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,21 +4,11 @@
 ;; ElDoc
 (add-hook 'python-mode-hook 'eldoc-mode)
 
-;; Package: pymacs
-(el-get-bundle pymacs)
-(add-to-list 'load-path "~/.emacs.d/el-get/pymacs")
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-(autoload 'pymacs-autoload "pymacs")
-
 ;; Package: ropemacs
-(el-get-bundle ropemacs)
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport t)
+;; (el-get-bundle ropemacs)
+;; (require 'pymacs)
+;; (pymacs-load "ropemacs" "rope-")
+;; (setq ropemacs-enable-autoimport t)
 
 ;; Package: elpy
 (el-get-bundle elpy)
