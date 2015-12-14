14045cbed59ee337112955e352bcf73fb133db48
Zhao Yun <yzhao.jon@wind.com.cn>
2015-12-14 17:13:06 +0800

    remove anaconda-mode, add pymacs, elpy and ropemacs

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,12 +4,26 @@
 ;; ElDoc
 (add-hook 'python-mode-hook 'eldoc-mode)
 
-;; Package: anaconda-mode
-(el-get-bundle anaconda-mode)
-(add-hook 'python-mode-hook 'anaconda-mode)
+;; Package: pymacs
+(el-get-bundle pymacs)
+(add-to-list 'load-path "~/.emacs.d/el-get/pymacs")
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
 
-;; Package: company-anaconda
-(el-get-bundle company-anaconda)
-(add-to-list 'company-backends 'company-anaconda)
+;; Package: ropemacs
+(el-get-bundle ropemacs)
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
+;; Package: elpy
+(el-get-bundle elpy)
+(elpy-enable)
+(when (executable-find "ipython")
+  (elpy-use-ipython))
 
 (provide 'init-python)
