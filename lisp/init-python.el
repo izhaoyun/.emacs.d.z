b118d0ea582be50bfc9de2a2e13d5e973ee0cb0c
Zhao Yun <zjlyzhy@gmail.com>
2015-12-25 09:08:43 +0800

    change elpy configuration.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,20 +1,19 @@
-;; Package: python.el
 (el-get-bundle python24)
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i")
 
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (setq-default indent-tabs-mode nil)
-             (setq default-tab-width 4)))
+(add-hook 'python-mode-hook '(lambda ()
+                               (setq-default indent-tabs-mode nil)
+                               (setq default-tab-width 4)))
 
 ;; ElDoc
 (add-hook 'python-mode-hook 'eldoc-mode)
 
 ;; Package: elpy
 (el-get-bundle elpy)
-(autoload 'elpy-enable "elpy" "enable elpy in all python buffers." t nil)
-(autoload 'elpy-mode "elpy" "minor mode in python buffers." t nil)
-(autoload 'elpy-use-ipython "elpy" "use ipython instead of python shell." t nil)
+(add-hook 'python-mode-hook '(lambda ()
+                               (elpy-enable)
+                               (elpy-mode)
+                               (elpy-use-ipython)))
 
 (provide 'init-python)
