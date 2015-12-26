b03cfe932f292f754c6ff7f78b4f5c48b0f8f375
Zhao Yun <zjlyzhy@gmail.com>
2015-12-26 11:10:54 +0800

    add swiper

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -6,11 +6,12 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 
-(global-set-key (kbd "M-x")     'helm-M-x)
+(global-set-key (kbd "C-c M-x")     'helm-M-x)
 (global-set-key (kbd "C-x b")   'helm-mini)
-(global-set-key (kbd "C-s")     'helm-occur)
+(global-set-key (kbd "C-c C-s")     'helm-occur)
+(global-set-key (kbd "C-c f")   'helm-recentf)
 (global-set-key (kbd "C-c h x") 'helm-register)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
 (global-set-key (kbd "M-y")     'helm-show-kill-ring)
@@ -46,5 +47,27 @@
 (el-get-bundle helm-descbinds)
 (require 'helm-descbinds)
 (helm-descbinds-mode 1)
+
+;; Package: swiper
+(el-get-bundle swiper)
+;; Package: swiper-helm
+;;(el-get-bundle swiper-helm)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+;; (global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-load-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c C-g") 'counsel-git)
+(global-set-key (kbd "C-c C-j") 'counsel-git-grep)
+(global-set-key (kbd "C-c C-s") 'counsel-ag)
+(global-set-key (kbd "C-c C-l") 'counsel-locate)
 
 (provide 'init-helm)
