13b7ed07ff9e6631c718713a47f974e7f0133466
Zhao Yun <zjlyzhy@gmail.com>
2015-12-27 01:55:16 +0800

    modify init-python.el

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,19 +1,7 @@
-(el-get-bundle python24)
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i"
-      python-indent-offset 4)
-
-(el-get-bundle init-python)
-
-;; ElDoc
-(add-hook 'python-mode-hook 'eldoc-mode)
-
 ;; Package: elpy
 (el-get-bundle elpy)
-(add-hook 'python-mode-hook '(lambda ()
-                               (elpy-enable)
-                               (elpy-mode)
-                               (setq elpy-rpc-backend "jedi")
-                               (elpy-use-ipython)))
+(elpy-enable)
+(setq elpy-rpc-backend "jedi")
+(elpy-use-ipython)
 
 (provide 'init-python)
