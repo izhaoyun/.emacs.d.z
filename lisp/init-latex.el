53cf8494ed12de3abdb38d9a2b6e29bb81a34016
Zhao Yun <zjlyzhy@gmail.com>
2016-01-03 18:16:09 +0800

    added autex configuration.

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -1,4 +1,12 @@
 ;; Package: auctex
 (el-get-bundle auctex)
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(setq TeX-command-default "XeLaTeX")
+(setq-default TeX-engine 'xetex)
 
 (provide 'init-latex)
