2eadab10fc6d522770ce4ef34977108ce1cf11e6
Zhao Yun <zjlyzhy@gmail.com>
2016-02-13 00:59:30 +0800

    add yasnippet

---
+++ init-editor/init-yasnippet.el
@@ -0,0 +1,8 @@
+(el-get-bundle yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"                 ;; personal snippets
+        "~/.emacs.d/el-get/yasnippet/snippets"         ;; the default collection
+        ))
+
+(yas-global-mode 1)
+
