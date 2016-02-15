49be8e5a8cdfe39b81356feb152b41c9b88f0215
Zhao Yun <zjlyzhy@gmail.com>
2016-02-15 21:07:37 +0800

    update init-session.el

--- settings/init-session.el
+++ settings/init-session.el
@@ -1,13 +1,14 @@
+;;; init-session.el --- Session management
+
+;;; Commentary:
+
+;;; Code:
+
+;; Package: workgroups2
 (el-get-bundle workgroups2)
 (require 'workgroups2)
 (setq wg-session-file "~/.emacs.d/.emacs_workgroups")
 (workgroups-mode 1)
 
-(el-get-bundle undo-tree)
-(global-undo-tree-mode 1)
-(setq undo-tree-visualizer-timestamps t
-      undo-tree-visualizer-diff       t)
-(defalias 'redo 'undo-tree-redo)
-(global-set-key (kbd "C-z") 'undo)
-
 (provide 'init-session)
+;;; init-session.el ends here
