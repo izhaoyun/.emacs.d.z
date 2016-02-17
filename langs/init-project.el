8c6bbb5fca3c8ce72c5b11fda49411e58e7ea365
Zhao Yun <zjlyzhy@gmail.com>
2016-02-17 17:23:03 +0800

    update highlight-indentation configuration

--- langs/init-project.el
+++ langs/init-project.el
@@ -62,6 +62,8 @@
 
 ;; Package: highlight-indentation
 (el-get-bundle highlight-indentation)
+(set-face-background 'highlight-indentation-face "#e3e3d3")
+(set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
 (add-hook 'python-mode-hook 'highlight-indentation-mode)
 (add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
 
