10f672c5ff3874418f115d0f3d9a7b3d3e60bc58
Zhao Yun <zjlyzhy@gmail.com>
2016-02-24 09:47:39 +0800

    add robe-mode and yari.

--- langs/init-ruby.el
+++ langs/init-ruby.el
@@ -13,5 +13,17 @@
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
 (inf-ruby-minor-mode +1)
 
+;; Package: robe
+(el-get-bundle robe-mode)
+(add-hook 'ruby-mode-hook 'robe-mode)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+
+;; Package: yari
+(el-get-bundle yari)
+(defun ri-bind-key ()
+  (local-set-key [f1] 'yari-anything))
+(add-hook 'ruby-mode-hook 'ri-bind-key)
+
 (provide 'init-ruby)
 ;;; init-ruby.el ends here
