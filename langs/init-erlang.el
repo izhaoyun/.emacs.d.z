0166b5f8803a3913070115ff351e6b26be150dc1
Zhao Yun <zjlyzhy@gmail.com>
2016-02-26 17:34:07 +0800

    add erlang-mode

--- langs/init-erlang.el
+++ langs/init-erlang.el
@@ -1,11 +1,10 @@
 ;;; init-erlang.el --- Configuration for Erlang
 
-
 ;;; Commentary:
-
+;; Package: erlang-mode
+(el-get-bundle erlang-mode)
 
 ;;; Code:
-
 
 (provide 'init-erlang)
 ;;; init-erlang.el ends here
