1b594538bc6d463096270fee771e1c9e05241a7b
Zhao Yun <zjlyzhy@gmail.com>
2016-02-27 09:57:36 +0800

    add init-web.el to init.el

--- langs/init-web.el
+++ langs/init-web.el
@@ -17,6 +17,16 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"         . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs$"         . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-css-indent-offset 2)
+            (setq web-mode-code-indent-offset 2)
+            (setq web-mode-enable-css-colorization t)))
 
 (provide 'init-web)
 ;;; init-web.el ends here
