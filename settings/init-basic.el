413c1e49320210e70003868b41efe9cf7fb93d0f
Zhao Yun <zjlyzhy@gmail.com>
2016-03-14 03:26:10 +0800

    add smartparens

--- settings/init-basic.el
+++ settings/init-basic.el
@@ -91,6 +91,62 @@
   (add-hook 'org-mode-hook #'rainbow-delimiters-mode)
   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
+;; Package: smartparens
+(el-get-bundle smartparens)
+;; https://github.com/sachac/.emacs.d/blob/gh-pages/Sacha.org#smartparens-mode
+(use-package smartparens
+  :config
+  (progn
+    (require 'smartparens-config)
+    (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+    (add-hook 'emacs-lisp-mode-hook 'show-smartparens-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;
+    ;; keybinding management
+    (define-key sp-keymap (kbd "C-c s r n") 'sp-narrow-to-sexp)
+    (define-key sp-keymap (kbd "C-M-f") 'sp-forward-sexp)
+    (define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
+    (define-key sp-keymap (kbd "C-M-d") 'sp-down-sexp)
+    (define-key sp-keymap (kbd "C-M-a") 'sp-backward-down-sexp)
+    (define-key sp-keymap (kbd "C-S-a") 'sp-beginning-of-sexp)
+    (define-key sp-keymap (kbd "C-S-d") 'sp-end-of-sexp)
+    (define-key sp-keymap (kbd "C-M-e") 'sp-up-sexp)
+    (define-key emacs-lisp-mode-map (kbd ")") 'sp-up-sexp)
+    (define-key sp-keymap (kbd "C-M-u") 'sp-backward-up-sexp)
+    (define-key sp-keymap (kbd "C-M-t") 'sp-transpose-sexp)
+    (define-key sp-keymap (kbd "C-M-n") 'sp-next-sexp)
+    (define-key sp-keymap (kbd "C-M-p") 'sp-previous-sexp)
+    (define-key sp-keymap (kbd "C-M-k") 'sp-kill-sexp)
+    (define-key sp-keymap (kbd "C-M-w") 'sp-copy-sexp)
+    (define-key sp-keymap (kbd "M-<delete>") 'sp-unwrap-sexp)
+    (define-key sp-keymap (kbd "M-<backspace>") 'sp-backward-unwrap-sexp)
+    (define-key sp-keymap (kbd "C-<right>") 'sp-forward-slurp-sexp)
+    (define-key sp-keymap (kbd "C-<left>") 'sp-forward-barf-sexp)
+    (define-key sp-keymap (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
+    (define-key sp-keymap (kbd "C-M-<right>") 'sp-backward-barf-sexp)
+    (define-key sp-keymap (kbd "M-D") 'sp-splice-sexp)
+    (define-key sp-keymap (kbd "C-M-<delete>") 'sp-splice-sexp-killing-forward)
+    (define-key sp-keymap (kbd "C-M-<backspace>") 'sp-splice-sexp-killing-backward)
+    (define-key sp-keymap (kbd "C-S-<backspace>") 'sp-splice-sexp-killing-around)
+    (define-key sp-keymap (kbd "C-]") 'sp-select-next-thing-exchange)
+    (define-key sp-keymap (kbd "C-<left_bracket>") 'sp-select-previous-thing)
+    (define-key sp-keymap (kbd "C-M-]") 'sp-select-next-thing)
+    (define-key sp-keymap (kbd "M-F") 'sp-forward-symbol)
+    (define-key sp-keymap (kbd "M-B") 'sp-backward-symbol)
+    (define-key sp-keymap (kbd "C-c s t") 'sp-prefix-tag-object)
+    (define-key sp-keymap (kbd "C-c s p") 'sp-prefix-pair-object)
+    (define-key sp-keymap (kbd "C-c s c") 'sp-convolute-sexp)
+    (define-key sp-keymap (kbd "C-c s a") 'sp-absorb-sexp)
+    (define-key sp-keymap (kbd "C-c s e") 'sp-emit-sexp)
+    (define-key sp-keymap (kbd "C-c s p") 'sp-add-to-previous-sexp)
+    (define-key sp-keymap (kbd "C-c s n") 'sp-add-to-next-sexp)
+    (define-key sp-keymap (kbd "C-c s j") 'sp-join-sexp)
+    (define-key sp-keymap (kbd "C-c s s") 'sp-split-sexp)
+;;;;;;;;;;;;;;;;;;
+    ;; pair management
+    (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
+    (sp-local-pair 'web-mode "<" nil :when '(my/sp-web-mode-is-code-context))))
+
+
 ;; https://www.emacswiki.org/emacs/ZapToChar
 ;; https://www.emacswiki.org/emacs/ZapToCharUsage
 (autoload 'zap-up-to-char "misc"
