b94de42a175f8425cf5908da4772bede7696e741
Zhao Yun <zjlyzhy@gmail.com>
2016-03-20 16:16:14 +0800

    update org-mode settings

--- modules/init-org.el
+++ modules/init-org.el
@@ -9,23 +9,55 @@
          ("C-c c" . org-capture)
          ("C-c l" . org-store-link))
   :init
-  (progn
-    (require 'org)
-    (require 'ox)
-    (require 'ox-beamer)
-    (require 'ox-latex)
-    (require 'ox-gfm))
+  (use-package org)
+  
   :config
+  ;; Withing with Code Blocks
+  (use-package ob :ensure nil
+    :config
+    (use-package ob-ditaa :ensure nil)
+    (use-package ob-plantuml :ensure nil)
+    (use-package ob-ruby :ensure nil)
+    (use-package ob-shell :ensure nil)
+    (use-package ob-python :ensure nil)
+    (use-package ob-sed :ensure nil)
+    (use-package ob-awk :ensure nil)
+    (use-package ob-latex :ensure nil)
+    (use-package ob-C :ensure nil)
+    (use-package ob-sql :ensure nil)
+    (use-package ob-dot :ensure nil)
+    )
+  
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (ditaa      . t)
+     (ruby       . t)
+     (python     . t)
+     (shell      . t)
+     (plantuml   . t)
+     (latex      . t)
+     (sed        . t)
+     (awk        . t)
+     (C          . t)
+     (sql        . t)
+     (dot        . t)))
 
-  ;; Package: org-bullets
-  (use-package org-bullets :defer t :load-path "src/org-mode/contrib/lisp"
+  ;;; Export Section
+  (use-package ox :ensure nil
     :init
-    (add-hook 'org-mode-hook 'org-bullets-mode))
+    (setq org-export-default-language "zh-CN")
+    :config
+    (use-package ox-beamer :ensure nil)
+    (use-package ox-md :ensure nil)
+    (use-package ox-gfm :ensure nil)
+    )
 
-  ;; Package: ob-ditaa
-  ;; (use-package ob-ditaa :defer t :load-path "src/org-mode/lisp")
 
-  ;; Package: htmlize
+  ;;; Others
+  (use-package org-bullets
+    :init (add-hook 'org-mode-hook 'org-bullets-mode))
+
   (use-package htmlize)
   )
 
