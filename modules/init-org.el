85bf3b7470a317876a2a0ecb80ede627db4d9f77
Zhao Yun <zjlyzhy@gmail.com>
2016-04-06 13:36:22 +0800

    remove linebreak for cjk

--- modules/init-org.el
+++ modules/init-org.el
@@ -77,21 +77,21 @@
               ))) ; ox-latex settings ends here.
 
     ;; ;; remove linebreak for cjk (http://zwz.github.io/)
-    ;; (defun clear-single-linebreak-in-cjk-string (string)
-    ;;   "clear single line-break between cjk characters that is usually soft line-breaks"
-    ;;   (let* ((regexp "\\([\u4E00-\u9FA5]\\)\n\\([\u4E00-\u9FA5]\\)")
-    ;;          (start (string-match regexp string)))
-    ;;     (while start
-    ;;       (setq string (replace-match "\\1\\2" nil nil string)
-    ;;             start (string-match regexp string start))))
-    ;;   string)
+    (defun clear-single-linebreak-in-cjk-string (string)
+      "clear single line-break between cjk characters that is usually soft line-breaks"
+      (let* ((regexp "\\([\u4E00-\u9FA5]\\)\n\\([\u4E00-\u9FA5]\\)")
+             (start (string-match regexp string)))
+        (while start
+          (setq string (replace-match "\\1\\2" nil nil string)
+                start (string-match regexp string start))))
+      string)
     
-    ;; (defun ox-html-clear-single-linebreak-for-cjk (string backend info)
-    ;;   (when (org-export-derived-backend-p backend 'html)
-    ;;     (clear-single-linebreak-in-cjk-string string)))
+    (defun ox-html-clear-single-linebreak-for-cjk (string backend info)
+      (when (org-export-derived-backend-p backend 'html)
+        (clear-single-linebreak-in-cjk-string string)))
     
-    ;; (add-to-list 'org-export-filter-final-output-functions
-    ;;              'ox-html-clear-single-linebreak-for-cjk)
+    (add-to-list 'org-export-filter-final-output-functions
+                 'ox-html-clear-single-linebreak-for-cjk)
     ))
 
 (use-package org-bullets
