b5fc5f0d09b3e563e4ac90a577316d22acd43478
Zhao Yun <zjlyzhy@gmail.com>
2016-05-12 01:01:06 +0800

    added helm-gtags for c/c++ programming.

--- langs/setup-cpp.el
+++ langs/setup-cpp.el
@@ -1,40 +1,34 @@
-(defun c++-config-default ()
-  "configuration for c/c++ programming."
-  
-  )
-
 (use-package cc-mode
   :mode (("\\.h\\'" . c++-mode))
   :init
-  (setq c-default-style "linux")
-  :config
-  )
-
-;;(use-package cmake-mode
-;;  :mode (("CMakeLists\\.txt\\'" . cmake-mode)
-;;         ("\\.cmake\\'"         . cmake-mode)))
+  (setq c-default-style "linux"))
 
 ;; (use-package company-c-headers
 ;;   :defer 8
 ;;   :config
 ;;   (add-to-list 'company-backends 'company-c-headers))
 
-;; (use-package helm-gtags
-;;   :defer 6
-;;   :init
-;;   (setq helm-gtags-ignore-case t)
-;;   (setq helm-gtags-auto-update t)
-;;   (setq helm-gtags-use-input-at-cursor t)
-;;   (setq helm-gtags-pulse-at-cursor     t)
-;;   (setq helm-gtags-suggested-key-mapping t)
-;;   (setq helm-gtags-prefix-key "\M-sg")
-;;   (add-hook 'c-mode-hook 'helm-gtags-mode)
-;;   (add-hook 'c++-mode-hook 'helm-gtags-mode)
-;;   (add-hook 'asm-mode-hook 'helm-gtags-mode)
-;;   :config
-;;   (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-;;   (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-;;   (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-;;   (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history))
+(use-package helm-gtags
+  :defer t
+  :init
+  (setq helm-gtags-ignore-case t)
+  (setq helm-gtags-auto-update t)
+  (setq helm-gtags-use-input-at-cursor t)
+  (setq helm-gtags-pulse-at-cursor     t)
+  (setq helm-gtags-suggested-key-mapping t)
+  (setq helm-gtags-prefix-key "\C-cg")
+  :bind
+  ("C-c g a" . helm-gtags-tags-in-this-function)
+  ("C-j" . helm-gtags-select)
+  ("M-." . helm-gtags-dwim)
+  ("M-," . helm-gtags-pop-stack)
+  ("C-c <" . helm-gtags-previous-history)
+  ("C-c >" . helm-gtags-next-history)
+  :config
+  (add-hook 'dired-mode-hook 'helm-gtags-mode)
+  (add-hook 'eshell-mode-hook 'helm-gtags-mode)
+  (add-hook 'c-mode-hook 'helm-gtags-mode)
+  (add-hook 'c++-mode-hook 'helm-gtags-mode)
+  (add-hook 'asm-mode-hook 'helm-gtags-mode))
 
 (provide 'setup-cpp)
