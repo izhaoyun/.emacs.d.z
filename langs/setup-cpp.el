7ca077be892e17604810888273639221fa154f02
Zhao Yun <zjlyzhy@gmail.com>
2016-05-14 00:26:32 +0800

    remove helm-gtags.

--- langs/setup-cpp.el
+++ langs/setup-cpp.el
@@ -35,27 +35,4 @@
               (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
                 (ggtags-mode 1)))))
 
-(use-package helm-gtags
-  :defer t
-  :init
-  (setq helm-gtags-ignore-case t)
-  (setq helm-gtags-auto-update t)
-  (setq helm-gtags-use-input-at-cursor t)
-  (setq helm-gtags-pulse-at-cursor     t)
-  (setq helm-gtags-suggested-key-mapping t)
-  (setq helm-gtags-prefix-key "\C-cg")
-  :bind
-  ("C-c g a" . helm-gtags-tags-in-this-function)
-  ("C-j" . helm-gtags-select)
-  ("M-." . helm-gtags-dwim)
-  ("M-," . helm-gtags-pop-stack)
-  ("C-c <" . helm-gtags-previous-history)
-  ("C-c >" . helm-gtags-next-history)
-  :config
-  (add-hook 'dired-mode-hook 'helm-gtags-mode)
-  (add-hook 'eshell-mode-hook 'helm-gtags-mode)
-  (add-hook 'c-mode-hook 'helm-gtags-mode)
-  (add-hook 'c++-mode-hook 'helm-gtags-mode)
-  (add-hook 'asm-mode-hook 'helm-gtags-mode))
-
 (provide 'setup-cpp)
