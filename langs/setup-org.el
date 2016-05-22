5974c1e28e04b6d5efe13d35d6635507b8c704ec
Zhao Yun <zjlyzhy@gmail.com>
2016-05-22 17:22:57 +0800

    disable org-bullets

--- langs/setup-org.el
+++ langs/setup-org.el
@@ -117,6 +117,26 @@ usually soft line-breaks"
   (setq org-src-tab-acts-natively t)
   ;; (add-hook 'org-mode-hook 'init-org-bullets)
 
+  ;; (font-lock-add-keywords 'org-mode
+  ;;                         '(("^ +\\([-*]\\) "
+  ;;                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+  ;; (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+  ;;                              ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+  ;;                              ((x-list-fonts "Verdana")         '(:font "Verdana"))
+  ;;                              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+  ;;                              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+  ;;        (base-font-color     (face-foreground 'default nil 'default))
+  ;;        (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+  ;;   (custom-theme-set-faces 'user
+  ;;                           `(org-level-8 ((t (,@headline ,@variable-tuple))))
+  ;;                           `(org-level-7 ((t (,@headline ,@variable-tuple))))
+  ;;                           `(org-level-6 ((t (,@headline ,@variable-tuple))))
+  ;;                           `(org-level-5 ((t (,@headline ,@variable-tuple))))
+  ;;                           `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+  ;;                           `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.35))))
+  ;;                           `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.55))))
+  ;;                           `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.7))))
+  ;;                           `(org-documentt-itle ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
   :config
   ;; alist of packages to be inserted in every LaTeX header.
   (add-to-list 'org-latex-packages-alist '("" "ctex"))
