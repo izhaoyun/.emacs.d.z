5e84ccd56a3c3473518b21f1330aff407fcd1429
Zhao Yun <zjlyzhy@gmail.com>
2016-05-24 16:23:13 +0800

    update setup-python.el

--- langs/setup-python.el
+++ langs/setup-python.el
@@ -1,11 +1,28 @@
+(defun python/init-pyenv-mode ()
+  (use-package pyenv-mode
+    :init
+    (pyenv-mode))
+  )
+
+(defun python/init-complete ()
+  (use-package anaconda-mode
+    :init
+    (anaconda-mode)
+    (anaconda-eldoc-mode))
+  (use-package company
+    :init
+    (setq company-show-numbers t)
+    (company-mode 1))
+  (use-package company-anaconda
+    :init
+    (eval-after-load 'company
+      '(push 'company-anaconda company-backends)))
+  )
+
 (use-package python
   :mode ("\\.py$" . python-mode)
   :init
-  (add-hook 'python-mode-hook 'init-auto-complete)
-  (use-package jedi
-    :init
-    (add-hook 'python-mode-hook 'jedi:setup)
-    (setq jedi:complete-on-dot t))
-  :config)
+  (add-hook 'python-mode-hook 'python/init-pyenv-mode)
+  (add-hook 'python-mode-hook 'python/init-complete))
 
 (provide 'setup-python)
