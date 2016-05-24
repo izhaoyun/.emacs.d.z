762100b01bbd1b66085b503d1df309df640eafcc
Zhao Yun <zjlyzhy@gmail.com>
2016-05-24 11:13:02 +0800

    update yasnippet settings.

--- core/init-develop.el
+++ core/init-develop.el
@@ -49,9 +49,19 @@
   (ws-butler-mode))
 
 (use-package yasnippet
-  :commands yas-global-mode
+  :commands (yas-reload-all
+             yas-minor-mode)
   :init
-  (yas-global-mode 1))
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook #'yas-minor-mode)
+  (eval-after-load 'company-mode
+    (use-package company-yasnippet
+      :ensure company
+      :commands company-yasnippet
+      :bind
+      ("C-c ; y" . company-yasnippet))
+    )
+  )
 
 (defun init-flycheck ()
   (use-package flycheck
@@ -73,52 +83,6 @@
   (setq projectile-indexing-method 'alien)
   (projectile-global-mode)
   )
-
-(defhydra hydra-projectile-other-window (:color teal)
-  "projectile-other-window"
-  ("f"  projectile-find-file-other-window        "file")
-  ("g"  projectile-find-file-dwim-other-window   "file dwim")
-  ("d"  projectile-find-dir-other-window         "dir")
-  ("b"  projectile-switch-to-buffer-other-window "buffer")
-  ("q"  nil                                      "cancel" :color blue))
-
-(defhydra hydra-projectile (:color teal
-                            :hint nil)
-  "
-     PROJECTILE: %(projectile-project-root)
-
-     Find File            Search/Tags          Buffers                Cache
-------------------------------------------------------------------------------------------
-_s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache clear
- _ff_: file dwim       _g_: update gtags      _b_: switch to buffer  _x_: remove known project
- _fd_: file curr dir   _o_: multi-occur     _s-k_: Kill all buffers  _X_: cleanup non-existing
-  _r_: recent file                                               ^^^^_z_: cache current
-  _d_: dir
-
-"
-  ("a"   projectile-ag)
-  ("b"   projectile-switch-to-buffer)
-  ("c"   projectile-invalidate-cache)
-  ("d"   projectile-find-dir)
-  ("s-f" projectile-find-file)
-  ("ff"  projectile-find-file-dwim)
-  ("fd"  projectile-find-file-in-directory)
-  ("g"   ggtags-update-tags)
-  ("s-g" ggtags-update-tags)
-  ("i"   projectile-ibuffer)
-  ("K"   projectile-kill-buffers)
-  ("s-k" projectile-kill-buffers)
-  ("m"   projectile-multi-occur)
-  ("o"   projectile-multi-occur)
-  ("s-p" projectile-switch-project "switch project")
-  ("p"   projectile-switch-project)
-  ("s"   projectile-switch-project)
-  ("r"   projectile-recentf)
-  ("x"   projectile-remove-known-project)
-  ("X"   projectile-cleanup-known-projects)
-  ("z"   projectile-cache-current-file)
-  ("`"   hydra-projectile-other-window/body "other window")
-  ("q"   nil "cancel" :color blue))
 
 (defun init-semantic-stickyfunc-enhance ()
   (use-package stickyfunc-enhance
