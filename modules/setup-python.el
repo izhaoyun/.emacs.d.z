9ede46f4f2e54bfef313ceffc667e06ef2143e5e
Yun Zhao <zjlyzhy@gmail.com>
2016-06-06 00:04:22 +0800

    disable sphinx-doc and enable virtualenvwrapper

--- modules/setup-python.el
+++ modules/setup-python.el
@@ -1,3 +1,14 @@
+(use-package python
+  :mode ("\\.py$" . python-mode)
+  :init
+  (setq python-indent-offset 4)
+  ;; (setq python-shell-interpreter "ipython")
+  ;; (setq python-shell-interpreter-args "")
+  ;; (setq python-shell-prompt-regexp "In \\[[0-9]+\\]: ")
+  ;; (setq python-shell-prompt-output-regexp  "Out\\[[0-9]+\\]: ")
+  :config
+  (use-package flycheck-pyflakes))
+
 (defun python/init-pyenv-mode ()
   (use-package pyenv-mode
     :init
@@ -5,18 +16,25 @@
   )
 (add-hook 'python-mode-hook 'python/init-pyenv-mode)
 
-;; (defun python/init-virtualenvwrapper ()
-;;   (use-package virtualenvwrapper
-;;     :config
-;;     (venv-initialize-interactive-shells)
-;;     (venv-initialize-eshell)))
-
-(defun python/init-sphinx-doc ()
-  (use-package sphinx-doc
+(defun python/init-virtualenvwrapper ()
+  (use-package virtualenvwrapper
+    :commands (venv-initialize-eshell
+               venv-initialize-interactive-shells)
     :init
-    (sphinx-doc-mode 1))
+    (venv-initialize-interactive-shells)
+    (venv-initialize-eshell)
+    :config
+    (setq eshell-prompt-function
+          (lambda () (concat venv-current-name " $ "))))
   )
-(add-hook 'python-mode-hook 'python/init-sphinx-doc)
+(add-hook 'python-mode-hook 'python/init-virtualenvwrapper)
+
+;; (defun python/init-sphinx-doc ()
+;;   (use-package sphinx-doc
+;;     :init
+;;     (sphinx-doc-mode 1))
+;;   )
+;; (add-hook 'python-mode-hook 'python/init-sphinx-doc)
 
 (defun python/init-complete ()
   (use-package anaconda-mode
@@ -39,17 +57,5 @@
     :init
     (fci-mode 1)))
 (add-hook 'python-mode-hook 'python/init-fill-column-indicator)
-
-(use-package python
-  :mode ("\\.py$" . python-mode)
-  :init
-  (setq python-indent-offset 4)
-  (setq python-shell-interpreter "ipython")
-  (setq python-shell-interpreter-args "")
-  (setq python-shell-prompt-regexp "In \\[[0-9]+\\]: ")
-  (setq python-shell-prompt-output-regexp  "Out\\[[0-9]+\\]: ")
-  :config
-  (use-package flycheck-pyflakes)
-  )
 
 (provide 'setup-python)
