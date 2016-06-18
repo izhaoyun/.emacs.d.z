6fa53511022059e7cd219770c0a9bf88c5b6216c
Zhao Yun <zjlyzhy@gmail.com>
2016-06-19 06:15:58 +0800

    add irony-mode and company-irony.

--- modules/setup-cpp.el
+++ modules/setup-cpp.el
@@ -2,6 +2,31 @@
   :mode (("CMakeLists.txt\\'" . cmake-mode)
          ("\\.cmake\\'" . cmake-mode)))
 
+(use-package irony-mode
+  :ensure irony
+  :init
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+  )
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+(defun cpp/init-company-irony ()
+  (use-package company-irony
+    :init
+    (push 'company-irony company-backends))
+  )
+(add-hook 'c-mode-common-hook 'cpp/init-company-irony)
+
 (defun cpp/init-company-c-headers ()
   (use-package company-c-headers
     :init
