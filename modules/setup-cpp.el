7bc3c1f93acd6451de66ed5bfe1192bc70c5e186
Zhao Yun <zjlyzhy@gmail.com>
2016-06-29 02:49:13 +0800

    remove irony

--- modules/setup-cpp.el
+++ modules/setup-cpp.el
@@ -2,35 +2,28 @@
   :mode (("CMakeLists.txt\\'" . cmake-mode)
          ("\\.cmake\\'" . cmake-mode)))
 
-(use-package irony-mode
-  :ensure irony
+(use-package ggtags
   :init
-  (add-hook 'c++-mode-hook 'irony-mode)
-  (add-hook 'c-mode-hook 'irony-mode)
-  (add-hook 'objc-mode-hook 'irony-mode)
+  (add-hook 'c-mode-common-hook 'ggtags-mode)
+  :bind
+  (:map ggtags-mode-map
+        ("C-c g s" . ggtags-find-other-symbol)
+        ("C-c g d" . ggtags-find-definition)
+        ("C-c g h" . ggtags-view-tag-history)
+        ("C-c g r" . ggtags-find-reference)
+        ("C-c g f" . ggtags-find-file)
+        ("C-c g c" . ggtags-create-tags)
+        ("C-c g u" . ggtags-update-tags)
+        ("C-," . pop-tag-mark)
+        ("M-." . ggtags-find-tag-dwim)
+        )
   :config
-  ;; replace the `completion-at-point' and `complete-symbol' bindings in
-  ;; irony-mode's buffers by irony-mode's function
-  (defun my-irony-mode-hook ()
-    (define-key irony-mode-map [remap completion-at-point]
-      'irony-completion-at-point-async)
-    (define-key irony-mode-map [remap complete-symbol]
-      'irony-completion-at-point-async))
-  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+  (setq-local eldoc-documentation-function #'ggtags-eldoc-function)
+  (setq-local hippie-expand-try-functions-list
+              (cons 'ggtags-try-complete-tag hippie-expand-try-functions-list))
   )
 
-(defun cpp/init-company-irony ()
-  (use-package company-irony
-    :init
-    (push 'company-irony company-backends))
-  )
-(add-hook 'c-mode-common-hook 'cpp/init-company-irony)
-
-(use-package irony-eldoc
-  :init
-  (add-hook 'irony-mode-hook 'irony-eldoc)
-  )
 
 (defun cpp/init-company-c-headers ()
   (use-package company-c-headers
@@ -39,10 +32,13 @@
   )
 (add-hook 'c-mode-common-hook 'cpp/init-company-c-headers)
 
-(use-package cmake-ide
-  :init
-  (cmake-ide-setup)
+(defun cpp/init-cmake-ide ()
+  (use-package cmake-ide
+    :init
+    (cmake-ide-setup)
+    )
   )
+(add-hook 'c-mode-common-hook 'cpp/init-cmake-ide)
 
 (use-package cc-mode
   :mode (("\\.h\\'" . c++-mode))
