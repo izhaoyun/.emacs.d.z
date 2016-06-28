2fc4620d21a1cdef5b26d09d342d3c78d368b14e
Zhao Yun <zjlyzhy@gmail.com>
2016-06-29 03:04:48 +0800

    use python-mode instead of python.el

--- modules/setup-python.el
+++ modules/setup-python.el
@@ -1,65 +1,12 @@
-(use-package python
+(use-package python-mode
+  :ensure python-mode
   :mode ("\\.py\\'" . python-mode)
-  :interpreter ("python" . python-mode)
+
   :init
-  (setq python-indent-offset 4)
+
   :config
-  (use-package flycheck-pyflakes))
 
-(defun python/init-pyenv-mode ()
-  (use-package pyenv-mode
-    :init
-    (pyenv-mode 1))
   )
-(add-hook 'python-mode-hook 'python/init-pyenv-mode)
-
-(defun python/init-nose ()
-  (use-package nose
-    :commands
-    :init
-    :config
-    (add-to-list 'nose-project-root-files "setup.cfg")
-    (setq nose-use-verbose nil))
-  )
-
-(defun python/init-virtualenvwrapper ()
-  (use-package virtualenvwrapper
-    :commands (venv-initialize-eshell
-               venv-initialize-interactive-shells)
-    :init
-    (venv-initialize-interactive-shells)
-    (venv-initialize-eshell)
-    :config
-    (setq eshell-prompt-function
-          (lambda () (concat venv-current-name " $ "))))
-  )
-(add-hook 'python-mode-hook 'python/init-virtualenvwrapper)
-
-;; (defun python/init-sphinx-doc ()
-;;   (use-package sphinx-doc
-;;     :init
-;;     (sphinx-doc-mode 1))
-;;   )
-;; (add-hook 'python-mode-hook 'python/init-sphinx-doc)
-
-(defun python/init-complete ()
-  (use-package anaconda-mode
-    :commands (anaconda-mode
-               anaconda-eldoc-mode)
-    :init
-    (anaconda-mode 1)
-    (anaconda-eldoc-mode 1))
-  (use-package company
-    :init
-    (company-mode 1)
-    :config
-    (setq company-show-numbers t))
-  (use-package company-anaconda
-    :init
-    (eval-after-load 'company
-      '(push 'company-anaconda company-backends)))
-  )
-(add-hook 'python-mode-hook 'python/init-complete)
 
 (defun python/init-fill-column-indicator ()
   (use-package fill-column-indicator
