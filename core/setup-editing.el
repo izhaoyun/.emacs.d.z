9ce24d313713d531e1444e2ef3c0124f470359e2
Zhao Yun <zjlyzhy@gmail.com>
2016-07-14 20:22:36 +0800

    set default file encoding to utf-8.

--- core/setup-editing.el
+++ core/setup-editing.el
@@ -14,6 +14,8 @@
 (global-linum-mode 1)
 (column-number-mode 1)
 
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
