af92149d029569001a200b28f710d9a2afdc7b18
Zhao Yun <zjlyzhy@gmail.com>
2016-07-18 14:31:50 +0800

    update which-key settings

--- init.el
+++ init.el
@@ -9,20 +9,27 @@
 (defvar use-package-verbose t)
 (require 'use-package)
 
+(require 'bind-key)
+(use-package diminish :load-path "site-lisp/diminish")
+
 (use-package avy
   :load-path "site-lisp/avy"
-  :bind
-  (("C-:" . avy-goto-char)
-   ("M-p" . avy-pop-mark))
-  :config
+  :commands (avy-setup-default)
+  :bind (("C-:" . avy-goto-char)
+	 ("M-p" . avy-pop-mark))
+  :init
   (avy-setup-default))
 
 (use-package which-key
   :load-path "site-lisp/which-key"
+  :commands (which-key-mode
+	     which-key-setup-side-window-right-bottom)
   :init
-  (which-key-mode))
+  (which-key-mode)
+  (which-key-setup-side-window-right-bottom))
 
 (use-package python-mode
   :load-path "site-lisp/python-mode"
   :config
   )
+
